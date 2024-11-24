0283d9d86fffee6444ce637cafd7d46cf17cd556
Benjie Gillam <benjie@jemjie.com>
2024-11-24 17:32:23 +0000

    Expose fieldName to filter resolve callbacks

--- src/PgConnectionArgFilterOperatorsPlugin.ts
+++ src/PgConnectionArgFilterOperatorsPlugin.ts
@@ -1213,7 +1213,11 @@ export interface OperatorSpec {
     sqlIdentifier: SQL,
     sqlValue: SQL,
     $input: InputStep,
-    $placeholderable: PlaceholderableStep
+    $placeholderable: PlaceholderableStep,
+    details: {
+      // TODO: move $input and $placeholderable here too
+      fieldName: string;
+    }
   ) => SQL;
   resolveType?: (type: GraphQLInputType) => GraphQLInputType;
 }
@@ -1265,6 +1269,7 @@ export function makeApplyPlanFromOperatorSpec(
   return EXPORTABLE(
     (
       connectionFilterAllowNullInput,
+      fieldName,
       lambda,
       resolve,
       resolveInput,
@@ -1335,11 +1340,14 @@ export function makeApplyPlanFromOperatorSpec(
       */
             $where.placeholder($resolvedInput, inputCodec);
 
-        const fragment = resolve(sqlIdentifier, sqlValue, $input, $where);
+        const fragment = resolve(sqlIdentifier, sqlValue, $input, $where, {
+          fieldName,
+        });
         $where.where(fragment);
       },
     [
       connectionFilterAllowNullInput,
+      fieldName,
       lambda,
       resolve,
       resolveInput,
